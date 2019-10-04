ae7b70516614f986446c6755399211dc3c753ad4
MinexAutomation <minex.automation@gmail.com>
2019-10-04 10:10:47 -0700

    Added example file paths and file names.

---
+++ source/R5T.Salamis/Code/ExampleFileNames.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+
+namespace R5T.Salamis
+{
+    public static class ExampleFileNames
+    {
+        public const string FileName1 = "File1.txt";
+    }
+}
---
+++ source/R5T.Salamis/Code/ExampleFilePaths.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+
+namespace R5T.Salamis
+{
+    public static class ExampleFilePaths
+    {
+        public const string WindowsFilePath1 = @"C:\Directory1\File1.txt";
+    }
+}
