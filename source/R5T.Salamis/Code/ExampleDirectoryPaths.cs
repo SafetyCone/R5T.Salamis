4d9ee84e1ef111565ac7b7054c5b7767b1a9bb35
MinexAutomation <minex.automation@gmail.com>
2019-10-09 16:20:47 -0700

    Added non-Windows paths.

--- source/R5T.Salamis/Code/ExampleDirectoryPaths.cs
+++ source/R5T.Salamis/Code/ExampleDirectoryPaths.cs
@@ -18,13 +18,19 @@ namespace R5T.Salamis
         public const string WindowsRootDirectoryPathUnindicated = @"C:"; // Also the same as the Windows volume path.
         public const string WindowsRootDirectoryPath = @"C:\";
 
-        public const string WindowsDirectory01FromWindowsDirectory04PathUnresolved = @"C:\Directory4\..\..\Directory1\";
+        public const string WindowsDirectory01FromWindowsDirectory04PathUnresolved = @"C:\Directory04\..\Directory01\";
 
         // Non-Windows.
         public const string NonWindowsDirectory01PathUnindicated = @"/mnt/Directory01";
         public const string NonWindowsDirectory01Path = @"/mnt/Directory01/";
-        public const string NonWindowsDirectory02PathUnindicated = @"/mnt/Directory02";
-        public const string NonWindowsDirectory02Path = @"/mnt/Directory02/";
+        public const string NonWindowsDirectory02PathUnindicated = @"/mnt/Directory01/Directory02";
+        public const string NonWindowsDirectory02Path = @"/mnt/Directory01/Directory02/";
+        public const string NonWindowsDirectory03PathUnindicated = @"/mnt/Directory01/Directory02/Directory03";
+        public const string NonWindowsDirectory03Path = @"/mnt/Directory01/Directory02/Directory03/";
+        public const string NonWindowsDirectory04PathUnindicated = @"/mnt/Directory01/Directory04";
+        public const string NonWindowsDirectory04Path = @"/mnt/Directory01/Directory04/";
+
+        public const string NonWindowsDirectory01FromNonWindowsDirectory04PathUnresolved = @"/mnt/Directory04/../Directory01/";
 
         // Mixed.
 
